a1f4d04cc8b67c1739bfa903a36ead575576e264
amupckuu <amupckuu@yandex.ru>
2026-01-22 09:10:21 +0300

    hw 7.1 * добавил @Nonnull аннотации в Config-ах

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/DockerConfig.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/DockerConfig.java
@@ -1,50 +1,64 @@
 package guru.qa.niffler.config;
 
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
 enum DockerConfig implements Config {
   INSTANCE;
 
+  @Nonnull
   @Override
   public String frontUrl() {
     return "";
   }
 
+  @Nonnull
   public String authUrl() {
     return "";
   }
 
+  @Nonnull
   @Override
   public String authJdbcUrl() {
     return "";
   }
 
+  @Nonnull
   public String gatewayUrl() {
     return "";
   }
 
+  @Nonnull
   public String userdataUrl() {
     return "";
   }
 
+  @Nonnull
   @Override
   public String userdataJdbcUrl() {
     return "";
   }
 
+  @Nonnull
   @Override
   public String spendUrl() {
     return "";
   }
 
+  @Nonnull
   @Override
   public String spendJdbcUrl() {
     return "";
   }
 
+  @Nonnull
   @Override
   public String currencyJdbcUrl() {
     return "";
   }
 
+  @Nonnull
   @Override
   public String githubUrl() {
     return "";
